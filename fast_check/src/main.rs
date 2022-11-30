c4d1a031afe7ca866a8c53c9468724297a37d81b
gopher <xiaogogonuo@163.com>
2022-11-30 22:06:10 +0800

    [@home]fixed hash_map entry

--- fast_check/src/main.rs
+++ fast_check/src/main.rs
@@ -1,18 +1,4 @@
-use std::collections::HashMap;
-
-fn main() {
-   let mut map: HashMap<i32, &str> = HashMap::new();
-    map.insert(1, "rust");
-    map.insert(2, "c++");
-    for (k, v) in &map {
-        if let Some(v) = map.get_mut(k) {
-            (*v).repeat(1);
-        }
-    }
-    dbg!(map);
-}
-
-
+fn main() {}
 
 // cargo build
 // objdump -d target/debug/fast_check > fast_check.s
