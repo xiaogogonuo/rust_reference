04e518ae992396652c282531400a3420106f9355
admin <admin@163.com>
2022-12-09 17:35:29 +0800

    [@com] add underscore case

--- underscore/src/lib.rs
+++ underscore/src/lib.rs
@@ -1,2 +1,16 @@
+//! # underscore: `_`
 
-// underscore: _
+pub mod underscore {
+    pub fn surname() -> (&'static str, &'static str) {
+        ("jordan", "michael")
+    }
+
+    pub fn applications() {
+        // ignore some value
+        let _ = "rust";
+
+        // ignore some value returned by function
+        let (_, surname) = surname();
+        assert_eq!(surname, "michael");
+    }
+}
