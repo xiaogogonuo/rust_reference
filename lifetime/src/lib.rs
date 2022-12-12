7d2f22062c6c31ab084bf96ade7cfe18754080cf
admin <admin@163.com>
2022-12-12 18:35:37 +0800

    [@com] add

--- lifetime/src/lib.rs
+++ lifetime/src/lib.rs
@@ -1,97 +1,50 @@
 //! # Lifetimes
 //!
-//! Lifetimes are another kind of generic. Rather than ensuring that a type has the behavior we
-//! want, lifetimes ensure that references are valid as long as we need them to be.
-//!
-//! Every reference in Rust has a lifetime, which is the scope for which that reference is valid.
-//!
-//! Most of the time, lifetimes are implicit and inferred. We only must annotate types when multiple
-//! types are possible.
-//!
-//! Rust requires us to annotate the relationships using generic lifetime parameters to ensure the
-//! actual references used at runtime will definitely be valid.
+//! Lifetimes are another kind of generic which ensure that references are valid. Every reference in
+//! Rust has a lifetime, which is the scope for which that reference is valid. Rust requires us to
+//! annotate the relationships using generic lifetime parameters to ensure the actual references
+//! used at runtime will definitely be valid.
 //!
 //! The main aim of lifetimes is to prevent dangling references.
 
 pub mod borrow_checker {
     //! The Rust compiler has a borrow checker that compares scopes to determine whether all borrows
     //! are valid.
-    //!
-    //! Here, we’ve annotated the lifetime of r with 'a and the lifetime of x with 'b. As you see,
-    //! the inner 'b block is much smaller than the outer 'a lifetime block. At compile time, Rust
-    //! compares the size of the two lifetimes and sees that r has a lifetime of 'a but that it
-    //! refers to memory with a lifetime of 'b. The program is rejected because 'b is shorter than
-    //! 'a: the subject of the reference does not live as long as the reference.
-    //!
-    //! ```panic
-    //! fn main() {
-    //!     let r;                // ---------+-- 'a
-    //!                           //          |
-    //!     {                     //          |
-    //!         let x = 5;        // -+-- 'b  |
-    //!         r = &x;           //  |       |
-    //!     }                     // -+       |
-    //!                           //          |
-    //!     println!("r: {}", r); // ---------+
-    //! }
-    //! ```
-    //!
-    //! Here, x has the lifetime 'b, which in this case is larger than 'a. This means r can
-    //! reference x because Rust knows that the reference in r will always be valid when x is valid.
-    //! ```
-    //! fn main() {
-    //!     let x = 5;            // ----------+-- 'b
-    //!                           //           |
-    //!     let r = &x;           // --+-- 'a  |
-    //!                           //   |       |
-    //!     println!("r: {}", r); // --+       |
-    //!                           //           |
-    //! }                         // ----------+
-    //! ```
-}
 
-pub mod generic_lifetime_in_function {
-    //! If we try to implement the longest function as shown, it won’t compile.
-    //!
-    //! ```panic
-    //! fn longest(x: &str, y: &str) -> &str {
-    //!     if x.len() > y.len() {
-    //!         x
-    //!     } else {
-    //!         y
-    //!     }
-    //! }
-    //! ```
-    //!
-    //! Instead, we get the following error that talks about lifetimes:
-    //! ```shell
-    //! $ cargo run
-    //!    Compiling chapter10 v0.1.0 (file:///projects/chapter10)
-    //! error[E0106]: missing lifetime specifier
-    //!  --> src/main.rs:9:33
-    //!   |
-    //! 9 | fn longest(x: &str, y: &str) -> &str {
-    //!   |               ----     ----     ^ expected named lifetime parameter
-    //!   |
-    //!   = help: this function's return type contains a borrowed value, but the signature does not
-    //! say whether it is borrowed from `x` or `y`
-    //! help: consider introducing a named lifetime parameter
-    //!   |
-    //! 9 | fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    //!   |           ++++     ++          ++          ++
-    //!
-    //! For more information about this error, try `rustc --explain E0106`.
-    //! error: could not compile `chapter10` due to previous error
-    //! ```
-    //!
-    //! The help text reveals that the return type needs a generic lifetime parameter on it because
-    //! Rust can’t tell whether the reference being returned refers to x or y.
-    //!
-    //! When we’re defining this function, we don’t know the concrete values that will be passed
-    //! into this function, so we don’t know whether the if case or the else case will execute.
-    //! We also don’t know the concrete lifetimes of the references that will be passed in, The
-    //! borrow checker can’t determine this either, because it does not know how the lifetimes of
-    //! x and y relate to the lifetime of the return value.
+    pub fn borrow_checker() {
+        {
+            let x: i8 = 1;     // ----------+-- 'b
+                               //           |
+            let r: &i8 = &x;   // --+-- 'a  |
+                               //   |       |
+            println!("{}", r); // --+       |
+        }                      // ----------+
+
+        {
+            let r: &i8;        // --+-- 'a
+            let x: i8 = 1;     // --|-------+-- 'b
+            r = &x;            //   |       |
+            println!("{}", r); // --+       |
+        }                      // ----------+
+
+        // panic case
+        // Here, we’ve annotated the lifetime of r with 'a and the lifetime of x with 'b.
+        // At compile time, Rust compares the size of the two lifetimes and sees that r has a
+        // lifetime of 'a but that it refers to memory with a lifetime of 'b. The program is
+        // rejected because 'b is shorter than 'a: the subject of the reference does not live as
+        // long as the reference.
+        {
+            // let r;                // ---------+-- 'a
+            //                       //          |
+            // {                     //          |
+            //     let x = 5;        // -+-- 'b  |
+            //     r = &x;           //  |       |
+            // }                     // -+       |
+            //                       //          |
+            // println!("r: {}", r); // ---------+
+            //
+        }
+    }
 }
 
 pub mod lifetime_annotation_syntax {
@@ -115,12 +68,29 @@ pub mod lifetime_annotation_syntax {
 }
 
 pub mod lifetime_annotation_in_function_signature {
+    //! ‼️ when needs lifetime annotations ‼️
+    //! Only when the function's return value contains reference.
+    //!
+    //! ‼️ how to use lifetime annotations ‼️
     //! To use lifetime annotations in function signatures, we need to declare the generic lifetime
     //! parameters inside angle brackets between the function name and the parameter list.
 
-    /// The `longest` function definition specifying that all the references in the signature must
-    /// have the same lifetime `'a`.
-    ///
+    pub fn no_need_lifetime_annotation(x: &str, y: &str) -> usize {
+        if x.len() > y.len() {
+            x.len()
+        } else {
+            y.len()
+        }
+    }
+
+    pub fn do_need_lifetime_annotation<'a>(x: &'a str, y: &'a str) -> &'a str {
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+
     /// We want the signature to express the following constraint: the returned reference will be
     /// valid as long as both the parameters are valid. This is the relationship between lifetimes
     /// of the parameters and the return value.
@@ -152,6 +122,97 @@ pub mod lifetime_annotation_in_function_signature {
     }
 }
 
+pub mod understanding_of_valid_reference {
+    //! ‼️ how to understand reference `valid` ‼️
+    //! The reference parameters are valid means that the value pointed to by reference is valid.
+
+    use super::lifetime_annotation_in_function_signature::longest;
+
+    pub fn right_nth1() {
+        let r: &str;                      // -----------------------------------+-- 'o: start
+                                          //                                    |
+        let s2: String;                   // ------------------+-- 'a: start   |
+                                          //                   |               |
+        let s1: String;                   // ------------------+-- 'b: start   |
+                                          //                   |               |
+        s1 = String::from("c++");      //                   |               |
+                                          //                   |               |
+        {                                 //                   |               |
+            s2 = String::from("rust"); //                   |               |
+                                          //                   |               |
+            r = longest(&s1, &s2);  //                   |               |
+        }                                 //                   |               |
+                                          //                   |               |
+        println!("{}", r);                // ------------------|---------------+ 'o: end
+                                          //                   |
+    }                                     //-------------------+ 'a 'b: end
+
+    /// Q: The returned reference will be valid as long as both the parameters are valid.
+    ///
+    /// A: The reference `r1` and `r2` are dropped when the inner scope ends, we can't invoke them
+    /// afterward, so `as long as both the parameters are valid` means that as long as the value
+    /// the references point to are valid. `s1` and `s2` are valid until the outer scope ends.
+    /// so reference `r` refers either `s1` or `s2`, both of `s1` and `s2` live outer of `r`.
+    pub fn right_nth2() {
+        let r: &str;                              // ----------------------------------+-- 'o: start
+                                                  //                                   |
+        let s1: String = String::from("rust"); // ------------------+-- 'a: start   |
+                                                  //                   |               |
+        let s2: String = String::from("c++");  // ------------------+-- 'b: start   |
+                                                  //                   |               |
+        {                                         //                   |               |
+            let r1: &str = s1.as_str();           // --+-- 'c: start   |               |
+                                                  //   |               |               |
+            let r2: &str = s2.as_str();           // --+-- 'd: start   |               |
+                                                  //   |               |               |
+            r = longest(r1, r2);            // --+ 'c 'd: end    |               |
+        }                                         //                   |               |
+                                                  //                   |               |
+        println!("{}", r);                        // ------------------|---------------+ 'o: end
+                                                  //                   |
+    }                                             // ------------------+ 'a 'b: end
+
+    pub fn right_nth3() {
+        let r: &str;                                 // ---------------------------------------------+-- 'o: start
+                                                     //                                              |
+        let s1: String = String::from("rust");    // -------------------------------+-- 'a: start |
+                                                     //                                |             |
+        {                                            //                                |             |
+            let s2: String = String::from("c++"); // -----------------+-- 'b: start |             |
+                                                     //                  |             |             |
+            let r1: &str = s1.as_str();              // --+-- 'c: start  |             |             |
+                                                     //   |              |             |             |
+            let r2: &str = s2.as_str();              // --+-- 'd: start  |             |             |
+                                                     //   |              |             |             |
+            r = longest(r1, r2);               // --+ 'c 'd: end   |             |             |
+                                                     //                  |             |             |
+            println!("{}", r);                       // -----------------|-------------|-------------+ 'o: end
+                                                     //                  |             |
+        }                                            // -----------------+ 'b: end     |
+                                                     //                                |
+    }                                                // -------------------------------+ 'a: end
+
+    pub fn error_nth1() {
+        let r: &str;                                 // ---------------------------------------------+-- 'o: start
+                                                     //                                              |
+        let s1: String = String::from("rust");    // -------------------------------+-- 'a: start |
+                                                     //                                |             |
+        {                                            //                                |             |
+            let s2: String = String::from("c++"); // -----------------+-- 'b: start |             |
+                                                     //                  |             |             |
+            let r1: &str = s1.as_str();              // --+-- 'c: start  |             |             |
+                                                     //   |              |             |             |
+            let r2: &str = s2.as_str();              // --+-- 'd: start  |             |             |
+                                                     //   |              |             |             |
+            r = longest(r1, r2);               // --+ 'c 'd: end   |             |             |
+        }                                            // -----------------+ 'b: end     |             |
+                                                     //                                |             |
+        // `s2` does not live long enough            //                                |             |
+        // println!("{}", r);                        // -------------------------------|-------------+ 'o: end
+                                                     //                                |
+    }                                                // -------------------------------+ 'a: end
+}
+
 pub mod thinking_in_terms_of_lifetime {
 
     /// If we changed the implementation of the `longest` function to always return the first
@@ -162,10 +223,10 @@ pub mod thinking_in_terms_of_lifetime {
     }
 }
 
-pub mod lifetime_annotation_in_struct {
+pub mod lifetime_annotation_in_struct_definitions {
     //! So far, the structs we’ve defined all hold owned types. We can define structs to hold
-    //! references, but in that case we would need to add a lifetime annotation on every reference
-    //! in the struct’s definition.
+    //! references, but we would need to add a lifetime annotation on every reference in the
+    //! struct’s definition.
 
     /// This annotation means an instance of `ImportantExcerpt` can’t outlive the reference it holds
     /// in its part field.
@@ -173,9 +234,20 @@ pub mod lifetime_annotation_in_struct {
         part: &'a str,
     }
 
-    pub fn struct_holds_reference_with_lifetime() {
-        let ie: ImportantExcerpt = ImportantExcerpt { part: "rust" };
+    pub fn right_nth1() {
+        let novel: String = String::from("rust will be the best language in the future.");
+        let ie: ImportantExcerpt = ImportantExcerpt { part: &novel[..4] };
         println!("{}", ie.part);
+    }
+
+    pub fn error_nth1() {
+        let ie: ImportantExcerpt;
+        {
+            let novel: String = String::from("rust will be the best language in the future.");
+            ie = ImportantExcerpt { part: &novel[..4] };
+        }
+        // error[E0597]: `novel` does not live long enough
+        // println!("{}", ie.part);
     }
 }
 
@@ -253,13 +325,5 @@ pub mod generic_type_trait_bound_lifetime {
         } else {
             y
         }
-    }
-}
-
-#[cfg(test)]
-mod testing {
-    #[test]
-    fn run_lifetime_annotation_in_struct_struct_holds_reference_with_lifetime() {
-        crate::lifetime_annotation_in_struct::struct_holds_reference_with_lifetime();
     }
 }
